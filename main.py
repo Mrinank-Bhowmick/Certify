24ca9aee261fe9fe6a453d177bde5edaaa7d42fa
Kokan <kokaipeter@gmail.com>
2022-10-06 13:06:39 +0200

    Refactor: use early return to reduce indentation
    Signed-off-by: Kokan <kokaipeter@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from PIL import Image, ImageDraw, ImageFont
 import os
 import csv
@@ -8,46 +9,46 @@ certificate_path = 'certificates/certificate.png'
 data_path = 'data/data.csv'
 
 def generate_certificate(name, course, date, certificate_number):
-    if os.path.isfile(template_path):
-        certificate = Image.open(template_path)
-        draw = ImageDraw.Draw(certificate)
-        font = ImageFont.truetype(font_path, size=50)
-        date_font = ImageFont.truetype(font_path, size=30)
-        certificate_number_font = ImageFont.truetype(font_path, size=30)
-
-        width, height = certificate.size
-
-        name_width, name_height = draw.textsize(name, font=font)
-
-        date_width, date_height = draw.textsize(date, font=date_font)
-
-        certificate_number_width, certificate_number_height = draw.textsize(certificate_number, font=certificate_number_font)
-
-        course_width, course_height = draw.textsize(course, font=font)
-
-        name_x = (width - name_width) / 2
-        name_y = (height - name_height) / 2
-
-        date_x = (width - date_width) / 2
-        date_y = (height - date_height) / 2 + 100
-
-        certificate_number_x = (width - certificate_number_width) / 2
-        certificate_number_y = (height - certificate_number_height) / 2 + 150
-
-        course_x = (width - course_width) / 2
-        course_y = (height - course_height) / 2 + 50
-
-        draw.text((name_x, name_y), name, fill='rgb(0, 0, 0)', font=font)
-
-        draw.text((date_x, date_y), date, fill='rgb(0, 0, 0)', font=date_font)
-
-        draw.text((certificate_number_x, certificate_number_y), certificate_number, fill='rgb(0, 0, 0)', font=certificate_number_font)
-
-        draw.text((course_x, course_y), course, fill='rgb(0, 0, 0)', font=font)
-
-        certificate.save(certificate_path)
-    else:
+    if not os.path.isfile(template_path):
         raise FileNotFoundError('No template')
+
+    certificate = Image.open(template_path)
+    draw = ImageDraw.Draw(certificate)
+    font = ImageFont.truetype(font_path, size=50)
+    date_font = ImageFont.truetype(font_path, size=30)
+    certificate_number_font = ImageFont.truetype(font_path, size=30)
+
+    width, height = certificate.size
+
+    name_width, name_height = draw.textsize(name, font=font)
+
+    date_width, date_height = draw.textsize(date, font=date_font)
+
+    certificate_number_width, certificate_number_height = draw.textsize(certificate_number, font=certificate_number_font)
+
+    course_width, course_height = draw.textsize(course, font=font)
+
+    name_x = (width - name_width) / 2
+    name_y = (height - name_height) / 2
+
+    date_x = (width - date_width) / 2
+    date_y = (height - date_height) / 2 + 100
+
+    certificate_number_x = (width - certificate_number_width) / 2
+    certificate_number_y = (height - certificate_number_height) / 2 + 150
+
+    course_x = (width - course_width) / 2
+    course_y = (height - course_height) / 2 + 50
+
+    draw.text((name_x, name_y), name, fill='rgb(0, 0, 0)', font=font)
+
+    draw.text((date_x, date_y), date, fill='rgb(0, 0, 0)', font=date_font)
+
+    draw.text((certificate_number_x, certificate_number_y), certificate_number, fill='rgb(0, 0, 0)', font=certificate_number_font)
+
+    draw.text((course_x, course_y), course, fill='rgb(0, 0, 0)', font=font)
+
+    certificate.save(certificate_path)
 
 def read_csv():
     with open(data_path, newline='') as csv_file:
