78122dcd8203b5d06c699351faf3a06374416572
Mrinank Bhowmick <77621953+Mrinank-Bhowmick@users.noreply.github.com>
2023-10-06 13:06:52 +0530

    Update fetch_avatars.py

--- fetch_avatars.py
+++ fetch_avatars.py
@@ -4,18 +4,39 @@ import requests
 def get_followers_avatar():
     repo = os.getenv("GITHUB_REPOSITORY")
     user = repo.split('/')[0]
-    followers_url = f"https://api.github.com/users/{user}/followers"
-    headers = {'Authorization': f'token {os.getenv("GH_TOKEN")}'}
-    response = requests.get(followers_url, headers=headers)
-    followers = response.json()
+    followers = []
+    page = 1
+    while True:
+        followers_url = f"https://api.github.com/users/{user}/followers?page={page}"
+        headers = {'Authorization': f'token {os.getenv("GH_PAT")}'}
+        response = requests.get(followers_url, headers=headers)
+        page_followers = response.json()
+        if not page_followers:
+            break
+        followers.extend(page_followers)
+        page += 1
 
     readme = open('README.md', 'w')
     readme.write("# My Followers\n\n")
-    readme.write("<style>\n.circle-avatar {\nborder-radius: 50%;\nwidth: 100px;\nheight: 100px;\n}\n</style>\n\n")
 
-    for follower in followers:
-        readme.write(f'<img class="circle-avatar" src="{follower["avatar_url"]}&s=100" alt="{follower["login"]}">\n')
+    readme.write("<table>\n<tr>\n")
+    for i, follower in enumerate(followers[:20]):
+        if i > 0 and i % 10 == 0:
+            readme.write("</tr>\n<tr>\n")
+        readme.write(f'<td align="center"><a href="https://github.com/{follower["login"]}"><img src="{follower["avatar_url"]}&s=50" width="50" height="50" alt="{follower["login"]}" /><br />@{follower["login"]}</a></td>\n')
+    readme.write("</tr>\n</table>")
 
+    readme.write("\n<details><summary>🔽 Show more</summary>\n\n")
+
+    readme.write("<table>\n<tr>\n")
+    for i, follower in enumerate(followers[20:]):
+        if i > 0 and i % 10 == 0:
+            readme.write("</tr>\n<tr>\n")
+        readme.write(f'<td align="center"><a href="https://github.com/{follower["login"]}"><img src="{follower["avatar_url"]}&s=50" width="70" height="70" alt="{follower["login"]}" /><br />@{follower["login"]}</a></td>\n')
+    readme.write("</tr>\n</table>")
+
+    readme.write("\n</details>")
+    
     readme.close()
 
 get_followers_avatar()
